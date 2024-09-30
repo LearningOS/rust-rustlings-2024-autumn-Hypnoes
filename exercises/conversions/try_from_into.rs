f338343a0bdabb6d146b4ee8daf9cacf5b1126ff
Hypnoes <liumulei@icloud.com>
2024-09-30 16:21:56 +0000

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,23 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn __is_valid_rgb(i: i16) -> bool {
+    i < 256 && i >= 0
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if __is_valid_rgb(tuple.0) && __is_valid_rgb((tuple.1)) && __is_valid_rgb((tuple.2)) {
+            Ok(Color {
+                red: tuple.0 as u8,
+                green: tuple.1 as u8,
+                blue: tuple.2 as u8,
+            })
+        } else {
+            Err(Self::Error::IntConversion)
+        }
     }
 }
 
@@ -48,6 +59,15 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if __is_valid_rgb((arr[0])) && __is_valid_rgb((arr[1])) && __is_valid_rgb((arr[2])) {
+            Ok(Color {
+                red: arr[0] as u8,
+                green: arr[1] as u8,
+                blue: arr[2] as u8,
+            })
+        } else {
+            Err(Self::Error::IntConversion)
+        }
     }
 }
 
@@ -55,6 +75,22 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(Self::Error::BadLen)
+        } else {
+            if __is_valid_rgb((slice[0]))
+                && __is_valid_rgb((slice[1]))
+                && __is_valid_rgb((slice[2]))
+            {
+                Ok(Color {
+                    red: slice[0] as u8,
+                    green: slice[1] as u8,
+                    blue: slice[2] as u8,
+                })
+            } else {
+                Err(Self::Error::IntConversion)
+            }
+        }
     }
 }
 
